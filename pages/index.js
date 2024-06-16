0d1d359b259f6c4b45977fac7a337530a8dd82c7
muknan <muk.msi@gmail.com>
2024-06-16 13:37:17 +0530

    Setting up dynamic routes with pages-router

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1>
-      Hello <strong>Pages</strong> Router
-    </h1>
+    <div>
+      <h1>
+        Hello <strong>Pages</strong> Router
+      </h1>
+      <div className="flex gap-12">
+        <Link href="/cabins">Cabins</Link>
+        <Link href="/about">About</Link>
+      </div>
+    </div>
   );
 }
